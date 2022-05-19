e1a5b44e44662750f4a0868459b5785364f237e3
star-moon-cloud-k <kspecial44@gmail.com>
2022-05-19 17:26:26 +0900

    boj1167

--- python/baekjoon_1157.py
+++ python/baekjoon_1157.py
@@ -2,7 +2,7 @@
 from collections import Counter
 
 input_counter = Counter(input().upper()) #대문자로 입력받고, 문자 수
-mc_results = input_counter.most_common(2)
+mc_results = input_counter.most_common(2) #튜플을 가진 리스트로 반환
 
 if len(mc_results) == 1:        #한 문자만 입력받았을 경우
     print(mc_results[0][0])
@@ -11,6 +11,3 @@ elif mc_results[0][1] != mc_results[1][1] and len(mc_results) >= 2:
     print(mc_results[0][0].upper())
 else:
     print("?")
-
-
-print(mc_results[0][0], mc_results[0][1])
