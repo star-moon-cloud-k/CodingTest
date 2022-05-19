8ef4f8fb4f831642e1b4fdaf5c4b016490229634
star-moon-cloud-k <kspecial44@gmail.com>
2022-05-19 17:26:14 +0900

    boj1167

--- python/baekjoon_1157.py
+++ python/baekjoon_1157.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 from collections import Counter
 
-input_counter = Counter(input().upper())
+input_counter = Counter(input().upper()) #대문자로 입력받고, 문자 수
 mc_results = input_counter.most_common(2)
 
-if mc_results[0][1] != mc_results[1][1]:
+if len(mc_results) == 1:        #한 문자만 입력받았을 경우
+    print(mc_results[0][0])
+    
+elif mc_results[0][1] != mc_results[1][1] and len(mc_results) >= 2:
     print(mc_results[0][0].upper())
 else:
     print("?")
+
+
+print(mc_results[0][0], mc_results[0][1])
