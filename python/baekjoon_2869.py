024c98d2025a3917edc953921b8ebadd50008dc2
star-moon-cloud-k <kspecial44@gmail.com>
2022-05-26 00:22:45 +0900

    boj2869

--- python/baekjoon_2869.py
+++ python/baekjoon_2869.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-
+import math
 up , sleep , height = map(int, input().split())
 
-go_up = 0
-day = 0
-for _ in range(height):
-    go_up += up
-    if go_up >= height:
-        day += 1
-        break
-    
-    go_up -= sleep
-    day += 1
 
-print(day) 
+# day = int(height // (up - sleep))
+# go_up = day - up + sleep
+# if (up) >= day :
+#     day += 1
+
+# else:
+#     day -= up
+#     for _ in range(height):
+#         go_up += up
+#         if go_up >= height:
+#             day += 1
+#             break
+#         go_up -= sleep
+#         day += 1
+
+day = (height - sleep) / (up - sleep)
+
+print(math.ceil(day)) 
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+# day = int(height // (up - sleep))
