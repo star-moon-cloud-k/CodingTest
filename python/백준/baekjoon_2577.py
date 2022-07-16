39f7ae64b35bf69c1c2686001983c06d1352dceb
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-16 23:44:19 +0900

    boj2577

--- python/백준/baekjoon_2577.py
+++ python/백준/baekjoon_2577.py
@@ -11,40 +11,45 @@
 # 출력
 # 첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다.
 #A , B , C  순서대로 입력
-from unittest.util import three_way_cmp
 
 
 A = int(input())
 B = int(input())
 C = int(input())
 R = A * B * C
+R = list(str(R))
+for i in range(10):
+    print(R.count(str(i)))
+    
+    
 #결과 string 값으로 변경
-result = list(str(R))
-for i in range(len(result)):
-    result[i] = int(result[i])
+# result = list(str(R))
+# for i in range(len(result)):
+#     result[i] = int(result[i])
 
-result_list = [0,0,0,0,0,0,0,0,0,0]
+# result_list = [0,0,0,0,0,0,0,0,0,0]
 
-for i in result:
-    if i == 0:
-        result_list[0] += 1
-    if i == 1:
-        result_list[1] += 1
-    if i == 2:
-        result_list[2] += 1
-    if i == 3:
-        result_list[3] += 1
-    if i == 4:
-        result_list[4] += 1
-    if i == 5:
-        result_list[5] += 1
-    if i == 6:
-        result_list[6] += 1
-    if i == 7:
-        result_list[7] += 1
-    if i == 8:
-        result_list[8] += 1
-    if i == 9:
-        result_list[9] += 1
-for i in result_list:
-    print(i)
+# for i in result:
+#     if i == 0:
+#         result_list[0] += 1
+#     if i == 1:
+#         result_list[1] += 1
+#     if i == 2:
+#         result_list[2] += 1
+#     if i == 3:
+#         result_list[3] += 1
+#     if i == 4:
+#         result_list[4] += 1
+#     if i == 5:
+#         result_list[5] += 1
+#     if i == 6:
+#         result_list[6] += 1
+#     if i == 7:
+#         result_list[7] += 1
+#     if i == 8:
+#         result_list[8] += 1
+#     if i == 9:
+#         result_list[9] += 1
+# for i in result_list:
+#     print(i)
+
