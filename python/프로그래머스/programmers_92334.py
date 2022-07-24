5713bfb2414a81389adc8a5df6c7483c28676c38
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-24 22:49:36 +0900

    신고 결과 받기_ 진행중

--- python/프로그래머스/programmers_92334.py
+++ python/프로그래머스/programmers_92334.py
@@ -1,3 +1,21 @@
 def solution(id_list, report, k):
+    
+    
+    banner_list = []
+    banned_list = []
+    report_data = []
+    report = set(report)
+    report = list(report)
+    for report_element in report:
+        #신고자 , 신고 당한 사람
+        banner, banned = report_element.split(" ")
+        banner_list.append(banner)
+        banned_list.append(banned)
+        report_data.append({banner : banned})
+        print(banner_list) , print(report_data)
+    
+    for id in id_list:
+        # if ()
+    
     answer = []
     return answer
