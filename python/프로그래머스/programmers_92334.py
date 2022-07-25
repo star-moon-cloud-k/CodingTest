3d4bb4a4c6b5a68c93738555cd4aa619bf4dabe7
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-25 20:16:51 +0900

    신고 결과 받기_ 진행중

--- python/프로그래머스/programmers_92334.py
+++ python/프로그래머스/programmers_92334.py
@@ -1,9 +1,9 @@
 def solution(id_list, report, k):
-    
-    
     banner_list = []
     banned_list = []
-    report_data = []
+    #id_list의 길이만큼 0으로 초기화
+    answer = [0 for i in range(len(id_list))]
+    
     report = set(report)
     report = list(report)
     for report_element in report:
@@ -11,11 +11,17 @@ def solution(id_list, report, k):
         banner, banned = report_element.split(" ")
         banner_list.append(banner)
         banned_list.append(banned)
-        report_data.append({banner : banned})
-        print(banner_list) , print(report_data)
+    
+ 
+    
+    #신고당한 id 수가 k보다 많은 경우 같은 인덱스에 k값, 아닌 경우 0
     
     for id in id_list:
-        # if ()
-    
-    answer = []
+        index = 0
+        if (k <= banned_list.count(id)):
+            for banned in banned_list:
+                if (id == banned):
+                    answer[id_list.index(banner_list[index])] += 1
+                index += 1
+                
     return answer
